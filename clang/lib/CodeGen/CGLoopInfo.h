aba150ff772bdc49a6fa25cfe53f80891f496278
m7241my <yuto.makino17@metro.co.jp>
2024-08-26 10:53:53 +0900

    refs #7147 Added changes regarding SWPL implementation.

--- clang/lib/CodeGen/CGLoopInfo.h
+++ clang/lib/CodeGen/CGLoopInfo.h
@@ -84,6 +84,12 @@ struct LoopAttributes {
 
   /// Value for whether the loop is required to make progress.
   bool MustProgress;
+
+  /// Value for llvm.loop.pipeline.enable metadata.
+  bool PipelineEnabled;
+
+  /// Value for llvm.loop.pipeline.nodep metadata
+  bool PipelineNodep;
 };
 
 /// Information used when generating a structured loop.
@@ -280,6 +286,9 @@ public:
   /// Set the pipeline disabled state.
   void setPipelineDisabled(bool S) { StagedAttrs.PipelineDisabled = S; }
 
+  /// Set the pipeline enabled state.
+  void setPipelineEnabled(bool S) { StagedAttrs.PipelineEnabled = S; }
+
   /// Set the pipeline initiation interval.
   void setPipelineInitiationInterval(unsigned C) {
     StagedAttrs.PipelineInitiationInterval = C;
@@ -290,6 +299,9 @@ public:
 
   /// Set no progress for the next loop pushed.
   void setMustProgress(bool P) { StagedAttrs.MustProgress = P; }
+
+/// Set the pipeline nodep
+  void setPipelineNodep(bool S) { StagedAttrs.PipelineNodep = S; }
 
 private:
   /// Returns true if there is LoopInfo on the stack.
