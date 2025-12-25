c8c529c7e8b21e2756ab63fd39e53e2ccc71989a
Sano Satomi <satomi@metro.co.jp>
2025-12-25 11:15:27 +0900

    refs #12569 Fix lit

--- clang/test/CodeGen/LoopDistribute4SWPL/ldist-loc003.c
+++ clang/test/CodeGen/LoopDistribute4SWPL/ldist-loc003.c
@@ -1,14 +1,6 @@
-// RUN: %clang --target=aarch64-linux-gnu -mcpu=a64fx -O3 -mllvm -vectorize-slp -mllvm -enable-loop-distribute4swpl -mllvm -distribute4swpl-limit-freg=1 -mllvm -distribute4swpl-limit-ireg=1 -fswp %s -c -o /dev/null -Rpass=.* -Rpass-missed=.* -Rpass-analysis=.* 2>&1 | FileCheck %s --check-prefix=RPASS
-// RPASS: ldist-loc003.c:20:3: ldist(2/3): remark: vectorized loop (vectorization width: vscale x 4, interleaved count: 1) [-Rpass=loop-vectorize]
-// RPASS: ldist-loc003.c:26:14: ldist(3/3): remark: Recipe with invalid costs prevented vectorization at VF=(vscale x 1): load [-Rpass-analysis=loop-vectorize]
-// RPASS:    26 |     D[i]   = H[i] * I[i];
-// RPASS:       |              ^
-// RPASS: ldist-loc003.c:26:14: ldist(3/3): remark: Cannot SLP vectorize list: vectorization was impossible with available vectorization factors [-Rpass-missed=slp-vectorizer]
-// RPASS:    26 |     D[i]   = H[i] * I[i];
-// RPASS:       |              ^
 // RUN: %clang --target=aarch64-linux-gnu -mcpu=a64fx -O3 -mllvm -vectorize-slp -mllvm -enable-loop-distribute4swpl -mllvm -distribute4swpl-limit-freg=1 -mllvm -distribute4swpl-limit-ireg=1 -fswp -fno-unroll-loops %s -g -S -o - | FileCheck %s --check-prefix=ASM
-// ASM:		.loc	{{[0-9]+}} 26 14 1                        //  clang/test/CodeGen/LoopDistribute4SWPL/ldist-loc003.c:26:14:ldsit(3/3)
-// ASM: 	ld1w	{ z0.s }, p1/z, [x16, x1, lsl #2]
+// ASM:		.loc	{{[0-9]+}} 13 19 is_stmt 0               // clang/test/CodeGen/LoopDistribute4SWPL/ldist-loc003.c:13:19 ldist(2/3)
+// ASM: 	ldur	s0, [x5]
 
 float target(int n,
             float *restrict A, float *restrict B, float *restrict C,
--- clang/test/CodeGen/LoopDistribute4SWPL/ldist-loc004.c
+++ clang/test/CodeGen/LoopDistribute4SWPL/ldist-loc004.c
@@ -1,8 +1,11 @@
 // RUN: %clang --target=aarch64-linux-gnu -mcpu=a64fx -O3 -mllvm -vectorize-slp -mllvm -enable-loop-distribute4swpl -mllvm -distribute4swpl-limit-freg=1 -mllvm -distribute4swpl-limit-ireg=1 -fswp -fno-vectorize -fno-unroll-loops %s -c -o /dev/null -Rpass=.* -Rpass-missed=.* -Rpass-analysis=.* 2>&1 | FileCheck %s --check-prefix=RPASS
-// RPASS: ldist-loc004.c:14:3: remark: distributed loop. num of distributied is 3. [-Rpass=loop-distribute4swpl]
-// RPASS: ldist-loc004.c:14:3: remark: loop not vectorized [-Rpass-missed=loop-vectorize]
-// RPASS: ldist-loc004.c:20:14: remark: Cannot SLP vectorize list: vectorization was impossible with available vectorization factors [-Rpass-missed=slp-vectorizer]
-// RPASS: ldist-loc004.c:14:3: remark: distributed loop: 1 of 3 software pipelining (IPC: 2.66, ITR: 10, MVE: 4, II: 3, Stage: 7, (VReg Fp: 13/32, Int: 7/29, Pred: 1/8)), SRA(PReg Fp: 9/32, Int: 5/29, Pred: 0/8) [-Rpass=aarch64-swpipeliner]
+// RPASS: ldist-loc004.c:17:3:ldist(3/3) remark: distributed loop. num of distributied is 3. [-Rpass=loop-distribute4swpl]
+// RPASS: ldist-loc004.c:17:3:ldist(2/3) remark: loop not vectorized [-Rpass-missed=loop-vectorize]
+// RPASS: ldist-loc004.c:17:3:ldist(1/3) remark: loop not vectorized [-Rpass-missed=loop-vectorize]
+// RPASS: ldist-loc004.c:17:3:ldist(3/3) remark: loop not vectorized [-Rpass-missed=loop-vectorize]
+// RPASS: ldist-loc004.c:17:3:ldist(3/3) remark: software pipelining (IPC: 2.66, ITR: 10, MVE: 4, II: 3, Stage: 7, (VReg Fp: 13/32, Int: 7/29, Pred: 1/8)), SRA(PReg Fp: 9/32, Int: 5/29, Pred: 0/8) [-Rpass=aarch64-swpipeliner]
+// RPASS: ldist-loc004.c:17:3:ldist(1/3) remark: software pipelining (IPC: 2.66, ITR: 10, MVE: 4, II: 3, Stage: 7, (VReg Fp: 13/32, Int: 7/29, Pred: 1/8)), SRA(PReg Fp: 9/32, Int: 5/29, Pred: 0/8) [-Rpass=aarch64-swpipeliner]
+// RPASS: ldist-loc004.c:17:3:ldist(2/3) remark: software pipelining (IPC: 3.00, ITR: 11, MVE: 4, II: 3, Stage: 8, (VReg Fp: 10/32, Int: 7/29, Pred: 1/8)), SRA(PReg Fp: 8/32, Int: 5/29, Pred: 0/8) [-Rpass=aarch64-swpipeliner]
 
 float target(int n,
             float *restrict A, float *restrict B, float *restrict C,
